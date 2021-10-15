189c6365d351e2effc04c522af862efdcef47980
Gumzzzi <shypo0@naver.com>
2021-10-15 13:05:35 +0900

    2021 10 15 테스트용 업로드 22

--- Camppy/src/co/yedam/camppy/Test.java
+++ Camppy/src/co/yedam/camppy/Test.java
@@ -3,6 +3,15 @@ package co.yedam.camppy;
 public class Test {
 
 	public void print(){
+
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
+		System.out.println("테스트입니다");
 		System.out.println("테스트입니다");
 		
 	}
