50773e927be11dd7e9e22c684e54bc51523173af
admin <admin@YD01-16>
2021-10-21 15:34:26 +0900

    최종 커밋

--- Camppy/WebContent/CCC.js
+++ Camppy/WebContent/CCC.js
@@ -4,9 +4,9 @@ function ScreenView(data) {
 
 	let divData = $('<div  />').addClass('data');
 	$(divData).append(
-		$('<div />').html('<img  style="display: block; margin: 0px auto;" width = "500px" src="./img2/' + data.i_info + '">'), //이미지 생성 
-		$('<div  />').html(data.i_name),  //상품이름
-		$('<div />').html(data.ori_p)  //상품가격
+		$('<div />').html('<a href="구매페이지.jsp"><img  style="display: block; margin: 0px auto;" width = "500px" src="./img2/' + data.i_info + '"></a>'), //이미지 생성 
+		$('<div style="text-align:center;" />').html(data.i_name),  //상품이름
+		$('<div style="text-align:center;" />').html(data.ori_p)  //상품가격
 	);
 	return divData;
 }
